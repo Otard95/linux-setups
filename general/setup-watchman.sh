303675f0e3578cb49fdc027ece3c6cb8d75e3c9e
StianM <stian.myklebostad@schibsted.com>
2020-03-03 08:40:30 +0100

    Add watchman install script

---
+++ general/setup-watchman.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pacman -S libssl-dev autoconf automake libtool # verify libssl-dev
+
+git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1
+cd watchman 
+./autogen.sh
+./configure
+make
+sudo make install
+
+cd ..
+rm -r watchman/
+
