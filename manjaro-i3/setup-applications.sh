af9ea45fe3ed7800bd3e5184628cb9fe906eb646
StianM <stian.myklebostad@schibsted.com>
2020-03-03 08:32:36 +0100

    Add install confirmation

--- manjaro-i3/setup-applications.sh
+++ manjaro-i3/setup-applications.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
 sudo pacman -Syu
-sudo pacman -S \
-  chromium \
-  code \
-  nodejs \
-  npm \
-  java-openjdk-bin
+
+packages=(
+"chromium" \
+"code" \ 
+"nodejs" \ 
+"npm" \ 
+"java-openjdk-bin" \
+)
+
+echo "Packages to be installed:"
+echo "${packages[@]}"
+printf "Do you want to procede with the install? [Y/n] "
+
+read ans
+
+case "$ans" in
+  "n"|"N"|"no"|"NO"|"No")
+    ;;
+  *)
+    echo ""
+    echo ""
+    sudo pacman -S "${packages[@]}"
+    ;;
+esac
+
