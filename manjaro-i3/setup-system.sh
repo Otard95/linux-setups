a8ce9a626ae32196614cf32f7fe65e8dbd916d82
StianM <stian.myklebostad@schibsted.com>
2020-03-03 08:33:13 +0100

    Add timezone selection form user

--- manjaro-i3/setup-system.sh
+++ manjaro-i3/setup-system.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+printf "Enter your timezone location: [Europe/Oslo] "
+read tz
+
+if "$tz" == ""; then
+  timezone="Europe/Oslo"
+else
+  timezone="$tz"
+
 sudo timedatectl set-ntp 1
-sudo timedatectl set-timezone "Europe/Oslo"
+sudo timedatectl set-timezone "$timezone"
 
 sudo cp ./09-timezone /etc/NetworkManager/dispatcher.d/
+sudo sed -i "s/Europe\\/Oslo/$timezone/g" /etc/NetworkManager/dispatcher.d/
 
 sudo cp ./xrandr-settings.desktop /etc/xdg/autostart/
 
