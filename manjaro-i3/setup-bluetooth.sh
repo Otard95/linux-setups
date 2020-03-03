db80d4cd76b50ea9199fd652baf330287ebe655f
StianM <stian.myklebostad@schibsted.com>
2020-03-03 11:07:05 +0100

    Fix indenting

--- manjaro-i3/setup-bluetooth.sh
+++ manjaro-i3/setup-bluetooth.sh
@@ -7,20 +7,20 @@ fi
 sudo systemctl start bluetooth
 
 if [ $? -eq 5 ]; then
-
-	sudo systemctl daemon-reload
-	sudo systemctl start bluetooth
-
-	if [ $? -eq 5 ]; then
-
-		echo "A restart may be required for bluetooth to properly setup."
-		echo "	After the you restart the system you can try to run the './setup-bluetooth.sh' script"
-
-		echo "btretry" >> ./rerun
-		exit 0
-
-	fi
-
+  
+  sudo systemctl daemon-reload
+  sudo systemctl start bluetooth
+  
+  if [ $? -eq 5 ]; then
+    
+    echo "A restart may be required for bluetooth to properly setup."
+    echo "	After the you restart the system you can try to run the './setup-bluetooth.sh' script"
+    
+    echo "btretry" >> ./rerun
+    exit 0
+    
+  fi
+  
 fi
 
 sudo systemctl enable bluetooth
