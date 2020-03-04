e449cc329c241305d1c110b8d955575809047a0d
StianM <stian.myklebostad@schibsted.com>
2020-03-04 14:55:26 +0100

    Confirm watchman install

--- general/setup-watchman.sh
+++ general/setup-watchman.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-sudo pacman -S libssl-dev autoconf automake libtool # verify libssl-dev
+printf "Do you want install watchman? [Y/n] "
 
-git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1
-cd watchman 
-./autogen.sh
-./configure
-make
-sudo make install
+read ans
 
-cd ..
-rm -r watchman/
+case "$ans" in
+  "n"|"N"|"no"|"NO"|"No")
+    ;;
+  *)
+    echo ""
+    echo ""
+    sudo pacman -S libssl-dev autoconf automake libtool setuptools python-dev libfolly # verify packages
+    
+    git clone https://github.com/facebook/watchman.git -b v4.9.0 --depth 1
+    cd watchman 
+    ./autogen.sh
+    ./configure
+    make
+    sudo make install
+    
+    cd ..
+    rm -r watchman/
+    ;;
+esac
+
+
 
