76c9321c25c2e22f97543d031ba929d9b6ea27d6
Stian Myklebostad <stian.myklebostad@getmail.no>
2020-03-09 09:57:23 +0100

    End if:

--- manjaro-i3/setup-system.sh
+++ manjaro-i3/setup-system.sh
@@ -3,16 +3,17 @@
 printf "Enter your timezone location: [Europe/Oslo] "
 read tz
 
-if "$tz" == ""; then
+if [ -z "$tz" ]; then
   timezone="Europe/Oslo"
 else
   timezone="$tz"
+fi
 
 sudo timedatectl set-ntp 1
 sudo timedatectl set-timezone "$timezone"
 
 sudo cp ./09-timezone /etc/NetworkManager/dispatcher.d/
-sudo sed -i "s/Europe\/Oslo/$timezone/g" /etc/NetworkManager/dispatcher.d/
+sudo sed -iE -e="s/Europe\/Oslo/$timezone/" /etc/NetworkManager/dispatcher.d/
 
-sudo cp ./xrandr-settings.desktop /etc/xdg/autostart/
+sudo cp ./xrandr-settings.desktop /etc/xdg/autostart
 
